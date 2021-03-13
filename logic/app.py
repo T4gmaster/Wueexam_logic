0c30e3049e69a336ef9b784d4babc570ecc04a3c
T4gmaster <lucbecker@gmx.de>
2021-03-13 16:30:48 +0100

    .

--- logic/app.py
+++ logic/app.py
@@ -179,10 +179,11 @@ def anzahl_studenten_10():
 
     if request.method == "GET":
         df = md.download_output("dataframe", table="enrollment_table")
-        df_grouped = df.groupby(["MATRICULATION_NUMBER","LAST_NAME","FIRST_NAME"]).size().reset_index(name='Anmeldungen')
-        datat_grouped2 = datat_grouped.rename(columns={"MATRICULATION_NUMBER":"Matrikelnummer","LAST_NAME":"Nachname","FIRST_NAME":"Vorname"})
-        students_over_10 = df_grouped[df_grouped["Anmeldungen"] > 5].sort_values(by="Anmeldungen", ascending = False)
-        json_students_over_10 = students_over_10.to_json(orient="records")
+        df_grouped = df.groupby(["MATRICULATION_NUMBER","LAST_NAME","FIRST_NAME"]).size().reset_index(name='Anmeldungen')   #grozup by the 3 columns and name the new one "Anmeldungen"
+        df_grouped2 = df_grouped.rename(columns={"MATRICULATION_NUMBER":"Matrikelnummer","LAST_NAME":"Nachname","FIRST_NAME":"Vorname"}) #rename the 3 first columns
+        
+        students_over_10 = df_grouped2[df_grouped2["Anmeldungen"] > 5].sort_values(by="Anmeldungen", ascending = False)               #order and filter the second grouped data
+        json_students_over_10 = students_over_10.to_json(orient="records")                  #convert to json
 
         return json_students_over_10
 
