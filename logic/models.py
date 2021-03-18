960b0b800adb99221644f4ffa1466f8de1fb0e7d
T4gmaster <lucbecker@gmx.de>
2021-03-18 15:21:42 +0100

    print json

--- logic/models.py
+++ logic/models.py
@@ -63,7 +63,8 @@ def upload_to_db(path: str, sql_table:str):
 def update_table(sql_table:str, type: str, json):
     """Update a table from a Frontend JSON Object entirely
     """
-    print(type(json),json)
+    print(json)
+    print(type(json))                             #delete after succesful testing
     df = pd.read_json(json, orient="records")
     print("json convertion worked in md.update_table") #delete after succesful testing
     dbf.write_df(sql_table, frame=df, type=type)
