ad634617f75f3682d5064983082cd610a42907bf
T4gmaster <lucbecker@gmx.de>
2021-03-19 17:43:06 +0100

    to_json

--- logic/models.py
+++ logic/models.py
@@ -64,7 +64,9 @@ def update_table(sql_table:str, type: str, json):
     """Update a table from a Frontend JSON Object entirely
     """
 
-    df = pd.read_json(json, orient="records")  #reocrds
+    json = pd.to_json(json, orient="records")
+
+    df = pd.read_json(json, orient="records")
 
     dbf.write_df(sql_table, frame=df, type=type)
 
