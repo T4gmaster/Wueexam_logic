fa188c27f55b348c137d6d3fb1c5d668ba425833
T4gmaster <lucbecker@gmx.de>
2021-03-19 19:00:59 +0100

    index 0

--- logic/models.py
+++ logic/models.py
@@ -68,7 +68,7 @@ def update_table(sql_table:str, type: str, json_file):
 
     df = pd.read_json(j, orient="records", typ="series")
     df.columns = ['EXAM', 'EXAM_ID',"LAST_NAME","FIRST_NAME","MATRICULATION_NUMBER","COURSE"]
-
+    df = df.drop(columns=["0"])
     dbf.write_df(sql_table, frame=df, type=type)
 
 ##########################################
