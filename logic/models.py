f04cc2cc3ea03255be942d1b14fda48a8724d4c3
T4gmaster <lucbecker@gmx.de>
2021-03-25 11:03:29 +0100

    timedelta

--- logic/models.py
+++ logic/models.py
@@ -166,8 +166,10 @@ def anzahl(frame, column:str):
 
 ##########################################
 def kalender_md(frame):
-    frame["start_date"] = frame["day_date"] + timedelta(hours=0)
-    frame["end_date"] = frame["day_date"] + timedelta(hours=3)      #exam takes 2 hours
+    frame["start_date"] = datetime.strptime(frame["day_date"],'%d-%m-%Y %H::%M')
+    #frame["start_date"] = frame["day_date"] + timedelta(hours=1)
+    #frame["start_date"] = frame["day_date"] - timedelta(hours=1)
+    frame["end_date"] = frame["day_date"] + timedelta(hours=2)      #exam takes 2 hours
 
     frame["start_date"] = frame["start_date"].astype(str)
     frame["end_date"]  = frame["end_date"].astype(str)
