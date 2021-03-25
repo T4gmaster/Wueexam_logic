5b3fcb1a9db76f9af6601d153ff15c08386e31d3
T4gmaster <lucbecker@gmx.de>
2021-03-25 11:10:03 +0100

    start date

--- logic/models.py
+++ logic/models.py
@@ -169,7 +169,7 @@ def kalender_md(frame):
     frame["start_date"] = datetime.strptime(frame["day_date"],'%d-%m-%Y %H::%M')
     #frame["start_date"] = frame["day_date"] + timedelta(hours=1)
     #frame["start_date"] = frame["day_date"] - timedelta(hours=1)
-    frame["end_date"] = frame["day_date"] + timedelta(hours=2)      #exam takes 2 hours
+    frame["end_date"] = frame["start_date"] + timedelta(hours=2)      #exam takes 2 hours
 
     frame["start_date"] = frame["start_date"].astype(str)
     frame["end_date"]  = frame["end_date"].astype(str)
